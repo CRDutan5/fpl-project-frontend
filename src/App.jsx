ed5f032edea93bb72bdd5a38f3c87c998102fc59
Carlitos <carlitosdutan@pursuit.org>
2025-01-02 14:44:55 -0500

    new inittt

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,9 @@
 import React, { useEffect, useState } from "react";
-import PlayerCard from "./PlayerCard";
 
 const App = () => {
-  const team_id_key = import.meta.env.VITE_MY_TEAM_ID;
-
-  const [myPlayers, setMyPlayers] = useState();
-
-  const [goalkeepers, setGoalkeepers] = useState();
-  const [defenders, setDefenders] = useState();
-  const [midfielders, setMidfielders] = useState();
-  const [forwards, setForwards] = useState();
-
-  useEffect(() => {
-    fetch(`http://127.0.0.1:5000/api/team/${team_id_key}/alternatives`)
-      .then((res) => res.json())
-      .then((data) => {
-        setMyPlayers(data);
-        setGoalkeepers(data.goalkeepers);
-        setDefenders(data.defenders);
-        setMidfielders(data.midfielders);
-        setForwards(data.forwards);
-      });
-  }, []);
-  // console.log(defenders);
   return (
-    <div className="">
-      {/* Goalkeepers */}
-      <div>
-        <div>
-          <h1 className="text-3xl">Goalkeepers:</h1>
-        </div>
-        <div>
-          {goalkeepers &&
-            goalkeepers.map((player) => (
-              <div key={player.my_player.id}>
-                <PlayerCard player={player} />
-              </div>
-            ))}
-        </div>
-      </div>
+    <div>
+      <h1>App</h1>
     </div>
   );
 };
